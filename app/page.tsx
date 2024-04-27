f00b6e59c285ddd345609a3dadf87f253118ad5d
phongvudz <nguyennguyen969873@gmail.com>
2024-04-28 00:00:22 +0700

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import { db } from "@/lib/db";
-import Image from "next/image";
-
 export default async function Home() {
-  const boards = await db.board.findMany();
-
-  console.log(boards);
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {boards.map((board) => (
-        <div key={board.id}>{board.id}</div>
-      ))}
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
